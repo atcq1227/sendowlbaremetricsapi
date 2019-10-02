941c41be2fc6282318301a8fccf28be13396f763
Matthew Shaw <>
2019-10-02 09:11:05 -0500

    Adding Baremetrics objects

---
+++ src/main/java/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+    String OID;
+    String sourceID;
+    String source;
+    String created;
+    String email;
+    String name;
+    String displayImagePath;
+    String displayName;
+    String notes;
+    int LTV;
+    boolean isActive;
+    boolean isCancelled;
+    String currentMRR;
+    Plan currentPlans;
+}
---
+++ src/main/java/Plan.java
@@ -0,0 +1,18 @@
+public class Plan {
+    String OID;
+    String sourceID;
+    String source;
+    String name;
+    String interval;
+    int intervalCount;
+    String trialDuration;
+    String trialDurationUnit;
+    String created;
+    boolean active;
+    int setupFees;
+    int LTV;
+    boolean isActive;
+    boolean isCancelled;
+    String currentMRR;
+    Plan currentPlans;
+}
